809cb6befda2073bcd7310a7c5e7a85ea33e8a10
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2026-06-20 00:56:37 -0600

    feat/cambios

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from 'astro:middleware'
 
 export const onRequest = defineMiddleware((_ctx, next) => {
   return next().then((response) => {
-    response.headers.set('Cache-Control', 'no-store')
+    response.headers.set('Cache-Control', 's-maxage=60, stale-while-revalidate=30')
     return response
   })
 })
